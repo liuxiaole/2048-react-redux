0722bbd0d4ecfeaa504914501faee56cbd29fe51
root <root@Will-PC.localdomain>
2020-09-21 14:34:25 +1200

    Updates

---
+++ precache-manifest.cefe6c989af1a71195c66b22168a467a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc883b341c86f2c06ce7347bf77c07c",
+    "url": "/2048-react-redux/index.html"
+  },
+  {
+    "revision": "9b270a68c6fad9f35f37",
+    "url": "/2048-react-redux/static/css/main.1427b515.chunk.css"
+  },
+  {
+    "revision": "4b8dd88fdd6033737fe1",
+    "url": "/2048-react-redux/static/js/2.4b7b1e12.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/2048-react-redux/static/js/2.4b7b1e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b270a68c6fad9f35f37",
+    "url": "/2048-react-redux/static/js/main.749f2fd6.chunk.js"
+  },
+  {
+    "revision": "2b797a8dc41e67a6761e",
+    "url": "/2048-react-redux/static/js/runtime-main.e523a3a3.js"
+  }
+]);
